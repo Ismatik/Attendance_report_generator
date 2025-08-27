2c2254495ba72f1584ad39572d899f2f529d1cd8
Ismatullo Mukhamedzhanov <mukhamejanov.ismat@gmail.com>
2025-08-27 18:12:37 +0500

    Add attendance report script

--- report_attendance.py
+++ report_attendance.py
@@ -13,20 +13,20 @@ for j in range(1,505):
   # Collect printed info into a list of dicts
   row_list = []
   for i in info_list:
-      print(f"PIN #{j}\n: {i['name']} {i['lastName']} {i['firstInTime']} {i['lastOutTime']} DEPARTMENT CODE {i['deptCode']}\n\n")
-      row_list.append({
-          "PIN": j,
-          "Name": i.get('name', ''),
-          "LastName": i.get('lastName', ''),
-          "FirstInTime": i.get('firstInTime', ''),
-          "LastOutTime": i.get('lastOutTime', ''),
-          "DeptCode": i.get('deptCode', '')
-      })
+    print(f"PIN #{j}\n: {i['name']} {i['lastName']} {i['firstInTime']} {i['lastOutTime']} DEPARTMENT CODE {i['deptCode']}\n\n")
+    row_list.append({
+        "PIN": j,
+        "Name": i.get('name', ''),
+        "LastName": i.get('lastName', ''),
+        "FirstInTime": i.get('firstInTime', ''),
+        "LastOutTime": i.get('lastOutTime', ''),
+        "DeptCode": i.get('deptCode', '')
+    })
 
   # Concatenate to overall_data
   if row_list:
-      df = pandas.DataFrame(row_list)
-      overall_data = pandas.concat([overall_data, df], ignore_index=True)
+    df = pandas.DataFrame(row_list)
+    overall_data = pandas.concat([overall_data, df], ignore_index=True)
 
 if not overall_data.empty:
   overall_data.to_excel("overall_attendance_report.xlsx", index=False)
