893cfec41bee6660e039ad3f0cb8f3d20cb3ddd5
Ismatullo Mukhamedzhanov <mukhamejanov.ismat@gmail.com>
2025-08-27 18:04:43 +0500

    Parsed info from skud to excel file

--- report_attendance.py
+++ report_attendance.py
@@ -2,24 +2,31 @@ import pandas
 import requests
 import openpyxl
 
-# url = "http://192.168.7.39:8098/api/v2/transaction/firstInAndLastOut/166?pageNo=1&pageSize=200&access_token=4ECD44C895F358C5DB39BC69C29B1006"# url = "http://192.168.7.39:8098/api/v2/transaction/firstInAndLastOut/166?pageNo=1&pageSize=200&access_token=4ECD44C895F358C5DB39BC69C29B1006"# url = "http://192.168.7.39:8098/api/v2/transaction/firstInAndLastOut/166?pageNo=1&pageSize=200&access_token=4ECD44C895F358C5DB39BC69C29B1006"
+overall_data = pandas.DataFrame()
+for j in range(1,505):
+  url = f"http://192.168.7.39:8098/api/v2/transaction/firstInAndLastOut/{j}?pageNo=1&pageSize=10&access_token=4ECD44C895F358C5DB39BC69C29B1006"
+  response = requests.get(url)
+  data = response.json()
+  info_list = data.get('data', {})
+  info_list = info_list.get('data', []) if isinstance(info_list, dict) else []
 
+  # Collect printed info into a list of dicts
+  row_list = []
+  for i in info_list:
+      print(f"PIN #{j}\n: {i['name']} {i['lastName']} {i['firstInTime']} {i['lastOutTime']} DEPARTMENT CODE {i['deptCode']}\n\n")
+      row_list.append({
+          "PIN": j,
+          "Name": i.get('name', ''),
+          "LastName": i.get('lastName', ''),
+          "FirstInTime": i.get('firstInTime', ''),
+          "LastOutTime": i.get('lastOutTime', ''),
+          "DeptCode": i.get('deptCode', '')
+      })
 
-# payload = ""
-# headers = {
-#   'Cookie': 'SESSION=N2RlMDA5MzQtMGIyYy00MTQ5LWE3NmQtM2Q5Y2ViZjdjN2Jk'
-# }
+  # Concatenate to overall_data
+  if row_list:
+      df = pandas.DataFrame(row_list)
+      overall_data = pandas.concat([overall_data, df], ignore_index=True)
 
-# response = requests.request("GET", url, headers=headers, data=payload)
-
-# data = response.json()
-# # If the response is like {"data": [...]}, extract the list
-# if isinstance(data, dict) and "data" in data:
-#     records = data["data"]
-# else:
-#     records = data
-
-# df = pandas.DataFrame(records)
-# df.to_excel("attendance_report.xlsx", index=False)
-# for i in df["data"]:
-#   print(i)
+if not overall_data.empty:
+  overall_data.to_excel("overall_attendance_report.xlsx", index=False)
